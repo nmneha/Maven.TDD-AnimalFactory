79e5519979aabc637dd804f862b649d1955952fe
Nusera <nusera@zipcoders-MBP.fios-router.home>
2022-03-06 19:23:39 -0500

    created and passed cathouse removeTest

--- src/test/java/rocks/zipcodewilmington/CatHouseTest.java
+++ src/test/java/rocks/zipcodewilmington/CatHouseTest.java
@@ -1,5 +1,12 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
+import org.junit.Test;
+import rocks.zipcodewilmington.animals.Cat;
+import rocks.zipcodewilmington.animals.animal_storage.CatHouse;
+
+import java.util.Date;
+
 /**
  * @author leon on 4/19/18.
  */
@@ -9,4 +16,29 @@ public class CatHouseTest {
     // TODO - Create tests for `void remove(Cat cat)`
     // TODO - Create tests for `Cat getCatById(Integer id)`
     // TODO - Create tests for `Integer getNumberOfCats()`
+
+    CatHouse catHouse = new CatHouse();
+
+    @Test
+    public void addTest() {
+        String expected = "Sew";
+        Cat cat = new Cat("Sew",new Date(),1920);
+        //When
+        catHouse.add(cat);
+        Cat actual = catHouse.getCatById(1920);
+
+        Assert.assertEquals(expected, actual.getName());
+    }
+
+    @Test
+    public void removeTest() {
+        Cat cat = new Cat(null,null,1920);
+        Integer expected = null;
+        catHouse.add(cat);
+        catHouse.remove(1920);
+        Cat actual = catHouse.getCatById(1920);
+
+        Assert.assertEquals(expected, actual);
+    }
+
 }
--- src/test/java/rocks/zipcodewilmington/DogTest.java
+++ src/test/java/rocks/zipcodewilmington/DogTest.java
@@ -2,7 +2,12 @@ package rocks.zipcodewilmington;
 
 import org.junit.Assert;
 import org.junit.Test;
+import rocks.zipcodewilmington.animals.Animal;
+import rocks.zipcodewilmington.animals.Cat;
 import rocks.zipcodewilmington.animals.Dog;
+import rocks.zipcodewilmington.animals.Mammal;
+
+import java.util.Date;
 
 /**
  * @author leon on 4/19/18.
@@ -15,6 +20,7 @@ public class DogTest {
     // TODO - Create tests for `Integer getId()`
     // TODO - Create test to check Animal inheritance; google search `java instanceof keyword`
     // TODO - Create test to check Mammal inheritance; google search `java instanceof keyword`
+    Dog dog = new Dog(null,null,null);
     @Test
     public void setNameTest() {
         // Given (a name exists and a dog exists)
@@ -28,4 +34,57 @@ public class DogTest {
         String dogName = dog.getName();
         Assert.assertEquals(dogName, givenName);
     }
+
+    @Test
+    public void speakTest() {
+        //Given
+        String expected = "bark!";
+        //When
+        String actual = dog.speak();
+        //Then
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void setBirthDateTest() {
+        //Given
+        Date expected = new Date(2001, 2, 23);
+        //When
+        dog.setBirthDate(expected);
+        Date actual = dog.getBirthDate();
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void eatTest() {
+        Integer eat = 1;
+        Food food = new Food();
+        //When
+        dog.eat(food);
+        Integer meals = dog.getNumberOfMealsEaten();
+
+        Assert.assertEquals(eat, meals);
+    }
+
+    @Test
+    public void getIdTest() {
+        Dog dog = new Dog(null,null, 831);
+        Integer id = 831;
+        Integer actual = dog.getId();
+
+        Assert.assertEquals(id, actual);
+    }
+
+    @Test
+    public void animalInheritance() {
+        Assert.assertTrue(dog instanceof Animal);
+    }
+
+    @Test
+    public void mammalInheritance() {
+        Assert.assertTrue(dog instanceof Mammal);
+    }
+
 }
